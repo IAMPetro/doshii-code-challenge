b511a97bb180af0e87767ed39803ed6889232fee
IAMPetro <petro.margaritis@gmail.com>
2017-01-10 21:05:56 +1100

    refactored for better performance for q1

--- 1_Efficiency/athletes.js
+++ 1_Efficiency/athletes.js
@@ -1,21 +1,35 @@
 'use strict'
 
-const fs = require('fs')
+const fs = require('fs'),
+    readline = require('readline');
+
+const GOLD_MEDAL_COLUMN = 9;
 
 module.exports = {
-  parseFile: function() {
-    return new Promise((resolve, reject) => {
-      let data, columns, i
-      let counter = 0
-      fs.readFile('./data/athletes.txt', 'utf8', (err, data) => {
-        if (err) throw new Error(err)
-        data = data.toString().split('\r\n')
-        for (i = 0; i < data.length  ; i++) {
-          columns = data[i].split(',')
-          if (columns[9]) counter += parseInt(columns[9])
-        }
-        resolve(counter)
-      })
-    })
-  }
+    parseFile: function () {
+        return new Promise((resolve, reject) => {
+            let data, columns;
+            let counter = 0;
+
+            var readStream = fs.createReadStream('./data/athletes.txt');
+
+            readStream.on('error', (err) => {
+                throw new Error(err)
+            });
+
+            var rd = readline.createInterface({
+                input: readStream,
+                output: process.stdout,
+                terminal: false
+            });
+
+            rd.on('line', (line) => {
+                columns = line.split(',')
+                if (columns[GOLD_MEDAL_COLUMN])
+                    counter += parseInt(columns[GOLD_MEDAL_COLUMN])
+            }).on('close', () => {
+                resolve(counter)
+            });
+        })
+    }
 }
